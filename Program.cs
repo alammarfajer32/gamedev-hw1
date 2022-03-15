5633c3325fc728114a91e8df54f951c9a8bdd56d
Fajer Alammar <alammarfajer@gmail.com>
2022-03-15 22:08:36 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int health = 200;
+
+            int damagedhealth = 20;
+
+            int boostedhealth = 10;
+
+            int damaged()
+            {
+
+                return health - damagedhealth;
+
+            }
+            int boosted()
+            {
+
+                return damaged() + boostedhealth;
+
+            }
+            int[] healtharray = { damaged(), boosted() };
+
+
+            void hp()
+            {
+                if (damaged() < boosted())
+                {
+                    Console.WriteLine(damagedhealth + " less than " + boostedhealth);
+                }
+
+                else if (damaged() > boosted())
+                {
+                    Console.WriteLine(damagedhealth + " greater than " + boostedhealth);
+                }
+                else
+                {
+                    Console.WriteLine(damagedhealth + " equal " + boostedhealth);
+                }
+            }
+            Console.WriteLine(" damaged health: " + damaged());
+            Console.WriteLine(" boosted health " + boosted());
+            Console.WriteLine(healtharray[1]);
+
+            hp();
+
+
+
+
+
+
+
+        }
+    }
+}
